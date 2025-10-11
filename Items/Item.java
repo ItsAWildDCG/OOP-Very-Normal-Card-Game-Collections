3efeae84ac6f791adb831afc1aada5e540dc050f
caccuhuy <caccuhuy@gmail.com>
2025-10-11 14:45:09 +0700

    Add files via upload

---
+++ Items/Item.java
@@ -0,0 +1,24 @@
+public  class Item {
+    private String ID, item_name, description;
+
+    public Item(String item_name){
+        this.item_name = item_name;
+    }
+
+    public String getName(){
+        return item_name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+    
+    public void when_is_used(){
+        System.out.println("When"); // will be changed later depend on the item
+    }
+    
+    public void use(){
+        System.out.println("What item?"); //same
+    }
+}
+
