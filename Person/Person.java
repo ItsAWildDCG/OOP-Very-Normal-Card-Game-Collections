fffb7596baa3a8dca217b9990bb51ae03d00f471
caccuhuy <caccuhuy@gmail.com>
2025-10-12 11:51:51 +0700

    Update Person.java

--- Person/Person.java
+++ Person/Person.java
@@ -11,15 +11,25 @@ public class Person {
     private int baseChips, currentChips;
     private List<Card> hand = new ArrayList<>();
     private Map<Item, Integer> items = new LinkedHashMap<>();
-    Person(){
+    
+    public Person(){
         this.CharName = "Jimbo";
     }
-    Person(String s){
+    public Person(String s){
         this.CharName = s;
     }
+
+    public int getBaseChips(){
+        return baseChips;
+    }
+
+    public int getCurrentChips(){
+        return currentChips;
+    }
+    
     void viewHand(){
         if (this.hand.isEmpty()){
-            System.out.println(this.CharName + "'hand is empty");
+            System.out.println(this.CharName + "'s hand is empty");
         }
         else{
             System.out.print(this.CharName + "'s hand: ");
@@ -40,7 +50,7 @@ public class Person {
     
     void drawFrom(Deck i){
         if(i.isEmpty()){
-            System.out.printl("Uh uh uh, ya can't draw from an empty deck ya know");
+            System.out.println("Uh uh uh, ya can't draw from an empty deck ya know");
         }
         else{
             this.hand.add(i.peek());
@@ -64,6 +74,7 @@ public class Person {
     }
 
     void decreaseItem(Item z){
+        if (items.get(z) == null) return;
         if(items.get(z) == 1){
             items.remove(z);
         else{
@@ -77,7 +88,7 @@ public class Person {
             decreaseItem(z);
         }
         else{
-            System.out.printl("What are you trying to do");
+            System.out.println("What are you trying to do");
         }
     }
 
@@ -99,6 +110,7 @@ public class Person {
     }
     
 }
+
 
 
 
