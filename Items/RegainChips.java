d66803180f33453d83017bcc1f4a7cb099272e35
caccuhuy <caccuhuy@gmail.com>
2025-10-12 11:52:50 +0700

    Update RegainChips.java

--- Items/RegainChips.java
+++ Items/RegainChips.java
@@ -5,12 +5,13 @@ import Person.Person;
 public class RegainChips extends Item {
     public RegainChips(String item_name){
         super(item_name);
-        ID = 1;
-        description = "A blessing from the Lord";
+        this.ID = 1;
+        this.description = "A blessing from the Lord";
     }
 
     @Override
     public void use(Person self){
-        self.healed(Math.round(baseChips / 5));
+        self.healed(Math.round(self.getBaseChips / 5));
     }
 }   
+
